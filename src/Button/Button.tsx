2043dca8fc1171e50a8bb47fd22b4a48cc387303
Rehane <abusayid693@gmail.com>
2022-03-27 09:40:56 +0530

    Compoent: Navigation props

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -2,11 +2,14 @@ import React, { HTMLAttributes, ReactNode } from 'react';
 import './style.css';
 
 interface Styles {
-  navigation?: Object;
-  navigationButton?: Object;
-  navigationIcon?: Object;
-  navigationBackground?: Object;
+  navigation?: React.CSSProperties;
+  navigationButton?: React.CSSProperties;
+  navigationIcon?: React.CSSProperties;
+  navigationBackground?: React.CSSProperties;
   floatButtonSize?: number;
+  floatButtonX?: string;
+  floatButtonY?: string;
+  listHoverColor?: [string, string];
 }
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
@@ -15,37 +18,47 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
 }
 
 export const Button = (props: Props) => {
+  const { styles, children } = props;
 
-  const {styles , children} = props
-
-  var cssVariables = { "--my-css-var": styles?.floatButtonSize ?? 1 } as React.CSSProperties;
+  var cssVariables = {
+    '--my-css-var': styles?.floatButtonSize ?? 1,
+    '--floatButtonX': styles?.floatButtonX ?? '85%',
+    '--floatButtonY': styles?.floatButtonY ?? '2%',
+    '--listHoverColor-1': styles?.listHoverColor?.[0] ?? 'transparent',
+    '--listHoverColor-2': styles?.listHoverColor?.[1] ?? '#fff',
+  } as React.CSSProperties;
 
   return (
-    <div className="navigation" {...props} style={styles?.navigation}>
+    <div
+      className="navigation"
+      {...props}
+      style={{
+        ...styles?.navigation,
+        ...cssVariables,
+      }}
+    >
       <input type="checkbox" className="navigation__checkbox" id="nav-toggle" />
       <label
         htmlFor="nav-toggle"
         className="navigation__button"
-        style={{ ...styles?.navigationButton, ...cssVariables } }
+        style={styles?.navigationButton }
       >
         <span
           className="navigation__icon"
-          style={props.styles?.navigationIcon}
+          style={styles?.navigationIcon}
         ></span>
         &nbsp;
       </label>
       <div
         className="navigation__background"
-        style={{
-          ...styles?.navigationBackground,
-          ...cssVariables
-        }}
+        style={styles?.navigationBackground
+        }
       >
         &nbsp;
       </div>
       <nav className="navigation__nav">
-        {props.children ? (
-          props.children
+        {children ? (
+          children
         ) : (
           <ul>
             <li>ABOUT</li>
