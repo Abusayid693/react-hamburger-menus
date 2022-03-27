77f16e79fff2df51991288393b2a3d4d4d7b6158
Rehane <abusayid693@gmail.com>
2022-03-27 08:09:56 +0530

    Compoent: Navigation styles

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,52 +1,50 @@
 import React, { HTMLAttributes, ReactNode } from 'react';
 import './style.css';
 
-
 interface Styles {
-  navigation?: Object
-  navigationButton?:Object
-  navigationIcon?:Object
-
+  navigation?: Object;
+  navigationButton?: Object;
+  navigationIcon?: Object;
+  navigationBackground?: Object;
 }
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactNode
-  styles?: Styles
+  children?: ReactNode;
+  styles?: Styles;
 }
 
 export const Button = (props: Props) => {
   return (
-    <div className="navigation" {...props} style={{
-
-    }}>
+    <div className="navigation" {...props} style={props.styles?.navigation}>
       <input type="checkbox" className="navigation__checkbox" id="nav-toggle" />
-      <label htmlFor="nav-toggle" className="navigation__button">
-        <span className="navigation__icon"></span>&nbsp;
+      <label
+        htmlFor="nav-toggle"
+        className="navigation__button"
+        style={props.styles?.navigationButton}
+      >
+        <span
+          className="navigation__icon"
+          style={props.styles?.navigationIcon}
+        ></span>
+        &nbsp;
       </label>
-      <div className="navigation__background">&nbsp;</div>
+      <div
+        className="navigation__background"
+        style={props.styles?.navigationBackground}
+      >
+        &nbsp;
+      </div>
       <nav className="navigation__nav">
-        <ul className="navigation__list">
-          <li className="navigation__item">
-            <a href="" className="navigation__link">
-              ABOUT
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="" className="navigation__link">
-              PROJECTS
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="" className="navigation__link">
-              EXPERIENCES
-            </a>
-          </li>
-          <li className="navigation__item">
-            <a href="" className="navigation__link">
-              BLOGS
-            </a>
-          </li>
-        </ul>
+        {props.children ? (
+          props.children
+        ) : (
+          <ul>
+            <li>ABOUT</li>
+            <li>PROJECTS</li>
+            <li>ELEMENTS</li>
+            <li>CONTACT</li>
+          </ul>
+        )}
       </nav>
     </div>
   );
