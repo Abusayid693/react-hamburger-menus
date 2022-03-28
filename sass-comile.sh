ff8dac6afb4b7a67bb853d72f85ab53f21ee2042
Rehane <abusayid693@gmail.com>
2022-03-28 17:51:41 +0530

    fix : minify command

--- sass-comile.sh
+++ sass-comile.sh
@@ -1,4 +1,4 @@
 for i in `ls -d ./src/*`
 do
-  (cd "$i" && echo "$i" && node-sass sass/style.scss style.css && cd ..)
+  (cd "$i" && echo "$i" && node-sass sass/style.scss style.css --output-style compressed && cd ..)
 done
